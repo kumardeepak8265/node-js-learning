c3f5873c8c52bc0b66b616896c98a54a026cf3ff
deepak kumar <kumardeepak6830@gmail.com>
2023-04-14 22:41:03 +0530

    day 15,16

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-const fs = require("fs");
-//fs ya node ka built module hai
-const path = require("path");
-//path hama path da deta hai ex= jasa hama kisi folder ka under jana hai tab ishka use karanga
-const dirPath = path.join(__dirname, "files");
-const filePath = `${dirPath}/apple.txt`;
-
-//files updated function
-// fs.appendFile(filePath, "and file name is apple.txt", (err) => {
-//   if (!err) {
-//     console.log("not error and file update");
-//   }
-// });
-
-//files rename
-// fs.rename(filePath, `${dirPath}/orange.txt`, (err) => {
-//   if (!err) {
-//     console.log("file update without error");
-//   }
-// });
-
-//files delete
-fs.unlinkSync(`${dirPath}/orange.txt`);
-
-//iq = buffer kya hota hai
-// node js ko file create karna ka liya space chiya hota hai isi ko buffer kahta hai
+// synchoronus and assynchronous
+let a = 10;
+let b = 20;
+const waitingData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(30);
+  }, 2000);
+});
+waitingData.then((data) => {
+  const b = data;
+  console.log("a+b", a + b);
+});
